852eefea15dfba3f027cfb931f594c8782591ba6
KingKaliente <142930185+KingKaliente@users.noreply.github.com>
2023-09-18 10:59:09 -0700

    PairProject.py

--- PairProject.py
+++ PairProject.py
@@ -1,7 +1,8 @@
-print("")
-userInput = input("Enter A or B. A) potato B) carrot")
+print("You've finally finished you're galatic adventure and finally head home to Earth. While exploring the universe, you find a single commonality. Potatoes. This gets you to wonder, do pigs prefer potatoes are carrots?")
+userInput = input("Enter A or B. A) Potatoes B) Carrots")
 
 if userInput == 'A':
+     print("Filled with anticipation, you decide to test your theory. Ultimately, you discover that pigs do in fact, prefer potatoes albeit you started The Great Potato Famine.")
      # Student 1 finishes this code
 
 elif userInput == 'B':
@@ -10,4 +11,4 @@ elif userInput == 'B':
 # Student 2 finishes this code 
 
 else: 
-     print("You entered something wrong - refresh and try again!")
+     print("You entered something wrong -  A potato flew around the room!")
