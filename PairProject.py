698d977ec8848b11421df45b3c1e3385ab11e0e5
KingKaliente <142930185+KingKaliente@users.noreply.github.com>
2023-09-18 10:56:24 -0700

    Update PairProject.py

--- PairProject.py
+++ PairProject.py
@@ -11,16 +11,3 @@ elif userInput == 'B':
 
 else: 
      print("You entered something wrong - refresh and try again!")
-print("")
-userInput = input("Enter A or B. A) potato B) carrot")
-
-if userInput == 'A':
-     # Student 1 finishes this code
-
-elif userInput == 'B':
-     print("You decided that pigs perfer carrots. To prove this you placed a carrot and a potato to two piles equal distance from the pigs to see how many pigs deside to go to that pile")
-     print("From this experiment you can say for sertin that pigs like carrots more. (to answer your dumb question)")
-# Student 2 finishes this code 
-
-else: 
-     print("You entered something wrong - refresh and try again!")
