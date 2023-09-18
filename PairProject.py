ef4d1496aba7148d9f00dfe42917378300a442ed
KingKaliente <142930185+KingKaliente@users.noreply.github.com>
2023-09-18 10:55:24 -0700

    Update PairProject.py

--- PairProject.py
+++ PairProject.py
@@ -1,10 +1,25 @@
-print("You are off on your space mission! You've just taken off and left the Earth's atmosphere. Your team is ready for an adventure of a lifetime! The goal of this mission is to try to find life in the universe. As the captain - you now have an important choice. Where do you go?")
-userInput = input("Enter A or B. A) Head towards Mars B) Head into empty space")
+print("")
+userInput = input("Enter A or B. A) potato B) carrot")
 
 if userInput == 'A':
-# Student 1 finishes this code
+     # Student 1 finishes this code
 
 elif userInput == 'B':
+     print("You decided that pigs perfer carrots. To prove this you placed a carrot and a potato to two piles equal distance from the pigs to see how many pigs deside to go to that pile")
+     print("From this experiment you can say for sertin that pigs like carrots more. (to answer your dumb question)")
+# Student 2 finishes this code 
+
+else: 
+     print("You entered something wrong - refresh and try again!")
+print("")
+userInput = input("Enter A or B. A) potato B) carrot")
+
+if userInput == 'A':
+     # Student 1 finishes this code
+
+elif userInput == 'B':
+     print("You decided that pigs perfer carrots. To prove this you placed a carrot and a potato to two piles equal distance from the pigs to see how many pigs deside to go to that pile")
+     print("From this experiment you can say for sertin that pigs like carrots more. (to answer your dumb question)")
 # Student 2 finishes this code 
 
 else: 
